95393064e7ce3c65316b88fa25ebf33ed57161ba
ASG <gulisikaynur@gmail.com>
2022-12-27 13:00:24 -0600

    page 4

---
+++ src/day2/Login.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class Login {
+    public static void main(String[] args) {
+      // page is created
+      // I am working on login page
+      // I am about complete
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
